e6dd6a287479aff9fc312c9ecce2c099da9de630
Mikhail Kuzin <mk@hobrum.com>
2021-02-03 23:11:34 +0300

    Modify main command

--- src/com/hobrum/mkadm/Main.java
+++ src/com/hobrum/mkadm/Main.java
@@ -1,10 +1,50 @@
 package com.hobrum.mkadm;
 
+import me.legrange.mikrotik.MikrotikApiException;
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MikrotikApiException {
 
+        boolean isExit = false;
+        
+        while (!isExit) {
 
+            System.out.println("Доступные команды:");
+            System.out.println("reboot one - Перезагрузить одно устройство. " +
+                    "Потребуется ввести ip адрес");
+            System.out.println("reboot all - Перезагрузить все устройства," +
+                    " которые указаны в файле .properties");
+            System.out.println("exit - завершить работу с программой");
+            System.out.println("Введите команду: ");
+
+            Scanner scanner = new Scanner(System.in);
+
+            switch (scanner.nextLine()) {
+                case "reboot one":
+                    MikrotikCommand mcOne = new MikrotikCommand();
+                    System.out.println("Введите ip: ");
+                    mcOne.rebootOneMikrotik(scanner.nextLine());
+                    break;
+
+                case "reboot all":
+                    MikrotikCommand mcAll = new MikrotikCommand();
+                    mcAll.rebootAllMikrotik();
+                    break;
+
+                case "exit":
+                    System.out.println("До свидания!");
+                    isExit = true;
+                    break;
+
+                default:
+                    System.out.println("Команда не найдена");
+                    System.out.println("");
+            }
+
+        }
 
     }
 
--- src/com/hobrum/mkadm/MikrotikCommand.java
+++ src/com/hobrum/mkadm/MikrotikCommand.java
@@ -1,5 +1,6 @@
 package com.hobrum.mkadm;
 
+import me.legrange.mikrotik.ApiConnection;
 import me.legrange.mikrotik.MikrotikApiException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,30 +10,39 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("com.hobrum.mkadm")
 public class MikrotikCommand {
 
-
-
-    /*public static void main(String[] args) throws MikrotikApiException {
+    public void rebootAllMikrotik() throws MikrotikApiException {
 
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(MikrotikList.class, MikrotikUser.class);
+
         MikrotikList ml = context.getBean("mikrotikList", MikrotikList.class);
-
         MikrotikUser mu = context.getBean("mikrotikUser", MikrotikUser.class);
-
 
         for (String l : ml.getIpMikrotikDevices()) {
 
             System.out.println(l);
             System.out.println(mu.getUserMikrotik());
             System.out.println(mu.getPasswordMikrotik());
-            ///ip/hotspot/walled-garden/add action=allow dst-host=*yandex.ru
 
-            //ApiConnection con = ApiConnection.connect(l);
-            //con.login(mu.getUserMikrotik(), mu.getPasswordMikrotik());
-            //con.execute("/system/reboot");
-            //con.execute("/ip/hotspot/walled-garden/add action=allow dst-host=*yandex.ru");
-            //con.close();
+            ApiConnection con = ApiConnection.connect(l);
+            con.login(mu.getUserMikrotik(), mu.getPasswordMikrotik());
+            con.execute("/system/reboot");
+            con.close();
         }
-    }*/
+    }
+
+    public void rebootOneMikrotik(String ipMikrotik) throws MikrotikApiException {
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MikrotikList.class, MikrotikUser.class);
+
+        MikrotikUser mu = context.getBean("mikrotikUser", MikrotikUser.class);
+
+            ApiConnection con = ApiConnection.connect(ipMikrotik);
+            con.login(mu.getUserMikrotik(), mu.getPasswordMikrotik());
+            con.execute("/system/reboot");
+            con.close();
+    }
+
 
 }
